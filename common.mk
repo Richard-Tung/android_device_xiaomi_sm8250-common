672aa50c12722b89968663785b413b8ea2b1fca7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-12-15 01:22:10 +0100

    sm8250-common: Import lights HAL
    * From LA.UM.9.12.r1-09300-SMxx50.0

--- common.mk
+++ common.mk
@@ -116,7 +116,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_kona
+    lights.xiaomi_kona
 
 # Media
 PRODUCT_COPY_FILES += \
--- lights/Android.mk
+++ lights/Android.mk
@@ -2,39 +2,25 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := android.hardware.light@2.0-service.xiaomi_kona
+LOCAL_MODULE := lights.xiaomi_kona
 LOCAL_MODULE_TAGS  := optional
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
+COMMON_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)
+LOCAL_MODULE_PATH_32 := $(COMMON_MODULE_PATH)/lib
+LOCAL_MODULE_PATH_64 := $(COMMON_MODULE_PATH)/lib64
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE_STEM := android.hardware.light@2.0-service
+LOCAL_INSTALLED_MODULE_STEM := lights.kona
+
+LOCAL_CFLAGS := -DLOG_TAG=\"qdlights\"
+LOCAL_CLANG  := true
 
 LOCAL_SRC_FILES := \
-    service.cpp \
-    Light.cpp
-
-LOCAL_REQUIRED_MODULES := \
-    android.hardware.light@2.0-service.xiaomi_kona.rc
+    lights.c
 
 LOCAL_SHARED_LIBRARIES := \
-    libbase \
-    libhardware \
-    libhidlbase \
-    liblog \
-    libutils \
-    android.hardware.light@2.0
+    liblog
 
-include $(BUILD_EXECUTABLE)
+LOCAL_HEADER_LIBRARIES := \
+    libhardware_headers
 
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := android.hardware.light@2.0-service.xiaomi_kona.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
-LOCAL_MODULE_STEM := android.hardware.light@2.0-service.rc
-
-LOCAL_SRC_FILES := android.hardware.light@2.0-service.xiaomi_kona.rc
-
-include $(BUILD_PREBUILT)
+include $(BUILD_SHARED_LIBRARY)
