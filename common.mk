crdroidandroid/android_device_oneplus_sm8250-common

sm8250-common: power: Rebase on LA.UM.9.12.r1-09000-SMxx50.0

common.mk
@@ -168,7 +168,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.oneplus_kona
+    android.hardware.power-service.oneplus_kona
 
 # PowerShare
 PRODUCT_PACKAGES += \

Richard-Tung/android_device_xiaomi_sm8250-common

Change-Id: Ie71ce52b50af2446e93f44cd545ff8462612c8d7

common.mk
@@ -148,7 +148,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.xiaomi_kona
+    android.hardware.power-service.xiaomi_kona
 
 # PowerShare
 PRODUCT_PACKAGES += \